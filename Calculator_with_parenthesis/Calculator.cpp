ad866f2626f780f82757a399e864be4cae184251
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2020-02-24 23:20:08 +0900

    Remakeing...

--- Calculator_with_parenthesis/Calculator.cpp
+++ Calculator_with_parenthesis/Calculator.cpp
@@ -1,1 +1,41 @@
 #include "Calculator.h"
+
+template<typename T>
+Calculator<T>::Calculator() {
+}
+
+template<typename T>
+Calculator<T>::~Calculator() {
+}
+
+template<typename T>
+bool Calculator<T>::IsOperand(char ch) {
+	return ('0' <= ch && ch <= '9');
+}
+
+template<typename T>
+bool Calculator<T>::IsOperator(char ch) {
+	const char operstors[] = "+-*/";
+	if (strchr(operators, ch))
+		return true;
+	return false;
+}
+
+template<typename T>
+int Calculator<T>::Prec(char ch) {
+	switch (char ch)
+	{
+	case '+':
+	case'-':
+		return 1;
+	case '*':
+	case'/':
+		return 2;
+	}
+	return -1;
+}
+
+template<typename T>
+bool Calculator<T>::InfixToPostfix(const char* exp) {
+
+}
--- Calculator_with_parenthesis/Calculator.h
+++ Calculator_with_parenthesis/Calculator.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <stack>
 
+template<typename T>
 class Calculator
 {
+	std::stack<T> stack;
+public:
+	Calculator();
+	~Calculator();
+
+	bool IsOperand(char ch);
+	bool IsOperator(char ch);
+
+	int Prec(char ch);
+
+	bool InfixToPostfix(const char* exp);
 };
